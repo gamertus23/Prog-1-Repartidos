a72a91bc027dc2186f278b073a7cec9b78248567
gamertus23 <rensiljo3@gmail.com>
2026-06-07 14:27:28 -0300

    mitad del 6

---
+++ 6.js
@@ -0,0 +1,13 @@
+/*6) Se tiene información de un censo. Cada familia encuestada tiene entre 0 y 12 hijos.
+Implementar una función que recibe un array donde cada posición contiene la cantidad de
+hijos de una familia y retorne un array donde cada posición es la cantidad total de familias
+con 12, con 11, ... , con 0 hijos */
+
+function calcTamañoFamilias (arr){
+    let resultado = new Array(13).fill(0);
+    
+
+}
+
+let senso = [3, 1, 2, 3, 0, 1]
+console.log(calcTamañoFamilias(senso))
