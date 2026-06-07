46fba637bedf020cdb29e35aa7566825965bb3c0
gamertus23 <rensiljo3@gmail.com>
2026-06-07 09:51:34 -0300

    4to

--- Repartido 8 - Arrays/4.js
+++ Repartido 8 - Arrays/4.js
@@ -4,6 +4,15 @@ sensores y retorna la cantidad de sensores que cumplen que él y sus dos vecinos
 prendidos
 Ejemplo con 10 sensores: (true, true, false, true, true, true, true, true, false, true), retorna 4 */
 function detectarSensores(){
-    let sensores = [];
-    
+    let sensores = [true, true, false, true, true, true, true, true, false, true];
+    let cantidad =0;
+    for(let i = 1 ; i < sensores.length - 1; i++){
+        if((sensores[i - 1]==sensores[i])&&(sensores[i]==sensores[i + 1])&&(sensores[i]==true)){
+            cantidad++;
+
+        }
+    }
+    return cantidad;
 }
+
+console.log(detectarSensores());
