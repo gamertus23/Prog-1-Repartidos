b03193e5e340f18249fb7083270bb93ab216a4bb
gamertus <rensiljo3@gmail.com>
2026-06-06 23:26:45 -0300

    ejercicio 1

--- Repartido 8 - Arrays/1.js
+++ Repartido 8 - Arrays/1.js
@@ -2,6 +2,19 @@
 valor dado. El más cercano puede ser un valor superior o inferior. Si hubiera más de un
 posible resultado, retornar cualquiera de ellos.
 Ejemplo: datos: 10 18 23 4 -5 50 , valor: 19, el más cercano es 18*/
-let datos = [10, 18, 23, 4, -5, 50];
+let datos=[ 10, 18, 23, 4, -5, 50];
 
-console.log(datos[2]);
+function numeroCercano (valor) {
+    let menor = Infinity;
+    let cualEsElMasCercano=0
+    for(let i = 0; i < datos.length ; i++){
+        let cerca = Math.abs(valor - datos[i]);
+        if (cerca < menor) {
+            menor = cerca;
+            cualEsElMasCercano = datos[i]
+        }
+    }
+return cualEsElMasCercano;
+}
+
+console.log(numeroCercano(19));
