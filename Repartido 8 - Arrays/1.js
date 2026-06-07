761920131fc81a8a5a7b9a33d100e03f458a4ea9
gamertus <rensiljo3@gmail.com>
2026-06-06 21:04:41 -0300

    a ver lo nuevo

---
+++ Repartido 8 - Arrays/1.js
@@ -0,0 +1,7 @@
+/*1) Dado un array y un valor, implementar una función que retorne el número más cercano al
+valor dado. El más cercano puede ser un valor superior o inferior. Si hubiera más de un
+posible resultado, retornar cualquiera de ellos.
+Ejemplo: datos: 10 18 23 4 -5 50 , valor: 19, el más cercano es 18*/
+let datos = [10, 18, 23, 4, -5, 50];
+
+console.log(datos[2]);
