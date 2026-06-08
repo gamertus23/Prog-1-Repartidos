47e8e82f4efa93a0271a323fb6090b3a56452cc2
gamertus23 <rensiljo3@gmail.com>
2026-06-08 08:30:42 -0300

    mitad del 7

---
+++ Repartido 8 - Arrays/7.js
@@ -0,0 +1,28 @@
+/*7) Se tiene la información de temperaturas de diversos puntos de control meteorológico del
+país. Esa información está en un array. Por cada medida, se indica punto de control (número
+entre 1 y 50) y temperatura observada.
+Ej.: datos: (1, 20, 4, 12, 1, 22, 3, 18, 4,16, 5,17, 8, 21,1, 21, 3, 6)
+Corresponde a: punto de control 1, temperatura 20; punto 4, temperatura 12; punto 1,
+temperatura 22, etc.
+Implementar una función que reciba un array cargado de esta manera y un valor y retorne
+un array con los puntos de control donde el respectivo promedio de temperatura sea menor
+que el valor dado. Para el ejemplo, si se indica valor 16, debe retornarse (3 4)
+Nota: solamente considerar aquellos puntos en los que se registraron datos. */
+
+function temperatura(arr, valor){
+    let contar = new Array(arr.length).fill(0);
+    let resultado = new Array(arr.length).fill(0);
+
+    for (let i = 0; i< arr.length -1; i+=2){
+        let pContro = arr[i]
+        let tempDeEsePt = arr[i + 1]
+        resultado[arr[i]]+=arr[i + 1]
+        contar[arr[i]]++
+    
+    }
+        
+} 
+
+
+let tempArr = [1, 20, 4, 12, 1, 22, 3, 18, 4,16, 5,17, 8, 21,1, 21, 3, 6];
+console.log(temperatura(tempArr, 16 ))
