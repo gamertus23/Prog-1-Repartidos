5a8570735b2187bdebc97341325eb2e71646065b
gamertus23 <rensiljo3@gmail.com>
2026-06-11 20:19:35 -0300

    ahora?

---
+++ Repartido 8 - Arrays/10.js
@@ -0,0 +1,6 @@
+/*10) Dado un array cargado con números y un valor, indicar en qué posiciones (índices) se
+encuentran dos elementos que sumados tienen como resultado el valor indicado. En caso de
+de haber más de un par, mostrar cualquiera de ellos. En caso de que no exista ningún par
+posible indicarlo.
+Ej: [23, 4, 56, 67, 8, 55] y valor: 64 mostrar “2 y 4”*/
+jhbgjhg
---
+++ Repartido 8 - Arrays/9.js
@@ -0,0 +1,5 @@
+/*9) Dado un array ordenado de números, implementar una función que retorne un nuevo array
+sin los elementos que estén duplicados en el array original.
+Ej: [1,1,56,67,67,101,156] retorna [1, 56, 67, 101, 156]*/
+
+function jhkhgk
