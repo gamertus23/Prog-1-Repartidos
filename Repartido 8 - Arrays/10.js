8e62b6c995b754633fcdfe1426e8d19a4142b05b
Renzo Amestoy <rensiljo3@gmail.com>
2026-06-11 21:58:45 -0300

    hola

--- Repartido 8 - Arrays/10.js
+++ Repartido 8 - Arrays/10.js
@@ -3,4 +3,25 @@ encuentran dos elementos que sumados tienen como resultado el valor indicado. En
 de haber más de un par, mostrar cualquiera de ellos. En caso de que no exista ningún par
 posible indicarlo.
 Ej: [23, 4, 56, 67, 8, 55] y valor: 64 mostrar “2 y 4”*/
-jhbgjhg
+
+function suma (arr, valor){
+    let v1 = -1;
+    let v2 = -1;
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 1; j < arr.length; j++ ){
+            if((arr[i]+arr[j]) == valor){
+                v1 = i;
+                v2 = j;
+            }        
+        }
+    }
+
+    if (v1 == -1 || v2 == -1){
+        return "no hay ningun par que sume " + valor
+    } else {
+        return v1+" y "+v2
+    }
+    
+}
+
+console.log(suma([23, 4, 56, 67, 8, 55], 64));
--- Repartido 8 - Arrays/9.js
+++ Repartido 8 - Arrays/9.js
@@ -2,4 +2,15 @@
 sin los elementos que estén duplicados en el array original.
 Ej: [1,1,56,67,67,101,156] retorna [1, 56, 67, 101, 156]*/
 
-function jhkhgk
+function modificarArr(arr){
+    let nuevoArr = [];
+    for (let i = 0; i < arr.length ; i++){
+        if (arr[i] !== arr[i + 1]){
+            nuevoArr.push(arr[i]);
+        }
+    }
+    return nuevoArr
+}
+
+let ejemplo = [1,1,56,67,67,101,156];
+console.log(modificarArr(ejemplo));
