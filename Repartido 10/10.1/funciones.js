cb0b05f56d39a42e39d8c0c5feb711ddcf669526
renzo linux mint <rensiljo3@gmail.com>
2026-07-01 21:36:11 -0300

    aaa

---
+++ Repartido 10/10.1/funciones.js
@@ -0,0 +1,9 @@
+let num1 = document.getElementById("n1");
+let num2 = document.getElementById("n2");
+
+addEventListener(click, function(
+    
+) )
+    let totalSuma = parseInt(num1) + parseInt(num1)
+
+    let totalProducto =  parseInt(num1) * parseInt(num1)
