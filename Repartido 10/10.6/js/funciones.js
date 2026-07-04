d0172f0b7c27de3de5d6ccb0328a5d8f4eea0a7a
Renzo Amestoy <rensiljo3@gmail.com>
2026-07-03 22:00:04 -0300

    fifa 26

--- Repartido 10/10.6/js/funciones.js
+++ Repartido 10/10.6/js/funciones.js
@@ -1,1 +1,29 @@
 /*6) Diseñar una página que ingrese un nombre, y si no está repetido, lo muestra en una lista */
+
+const { createElement } = require("react");
+
+let nombres = [];
+window.addEventListener("load", inicio);
+
+function inicio(){
+    document.getElementById("siguiente").addEventListener("click", agregarNombre);
+}
+
+function agregarNombre() {
+    let nombreIngresado = document.getElementById("nombre").value.trim();
+    if (nombres.includes(nombreIngresado)){
+        alert("Este nombre ya existe");
+    } else {
+        nombres.push(nombreIngresado);
+        agregarEnLaLista(nombreIngresado);
+    }
+    document.getElementById("nombre").value = "";
+}
+
+function (agregarEnLaLista){
+    let lista = document.getElementById("lista");
+    let li = document.createElement("li");
+    let unNombre = document.createTextNode(nombre);
+    li.appendChild(unNombre);
+    lista.appendChild(li);      
+}
